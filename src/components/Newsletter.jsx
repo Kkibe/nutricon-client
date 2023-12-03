a2d474add5a2a52538312e2567897c71aa70de1f
Kkibe <137826199+Kkibe@users.noreply.github.com>
2023-12-04 02:34:26 +0300

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -6,7 +6,7 @@ function Newsletter() {
         <form>
           <h3 className='h3'>Subscribe to our newsletter</h3>
           <p className='lead'>Monthly digest of what's new and exciting from us.</p>
-          <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+          <div className="d-flex flex-sm-row w-100 gap-2">
             <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
             <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
             <button className="btn btn-success" type="button">Subscribe</button>
