69ce000846ba08fa6cd0389f665a40126c9ca1f3
Kkibe <137826199+Kkibe@users.noreply.github.com>
2023-12-05 02:06:32 +0300

    Update Topnav.jsx

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -15,7 +15,7 @@ function Topnav() {
         <img src={Icon} alt="mdo" width="32" height="32" className="rounded-circle border me-2" />
         <span className="fs-4 text-success lead">Nutricon</span>
       </a>
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 nav-pills   d-none d-lg-block d-xl-none">
+        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 nav-pills">
           <li className="nav-item mx-2"><NavLink to='/' className='nav-link px-2'  title='home'>Home</NavLink></li>
           <li className="nav-item mx-2"><NavLink to='/services' className='nav-link px-2'  title='services'>Services</NavLink></li>
           <li className="nav-item mx-2"><NavLink to='/blogs' className='nav-link px-2' title='blogs'>Blogs</NavLink></li>
@@ -36,7 +36,7 @@ function Topnav() {
             </ul>
           </div>
           ) : (
-            <div className="text-end d-none d-lg-block d-xl-none">
+            <div className="text-end">
               <a href='/login' className="btn btn-outline-success me-2" role='button'>Login</a>
               <a href='/register' className="btn btn-outline-secondary me-2 " role='button'>Register</a>
           </div>
