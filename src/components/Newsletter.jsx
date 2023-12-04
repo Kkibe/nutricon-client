a9e8e18fa6eb426813d11a8920ff3475b1d48d90
Kkibe <137826199+Kkibe@users.noreply.github.com>
2023-12-04 23:32:10 +0300

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Newsletter() {
   return (
-    <div className='d-flex justify-content-center align-items-center py-5 my-3 bg-light'>
+    <div className='d-flex justify-content-center align-items-center py-5 bg-light'>
         <form>
           <h3 className='h3'>Subscribe to our newsletter</h3>
           <p className='lead'>Monthly digest of what's new and exciting from us.</p>
-          <div className="d-flex flex-sm-row w-100 gap-2">
+          <div className="d-flex flex-sm-row w-100 gap-2 custom-file py-5">
             <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
             <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
             <button className="btn btn-success" type="button">Subscribe</button>
