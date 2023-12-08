2ab3272f8c02a6aaa6cfa813c0c2a25af673ccff
Kkibe <137826199+Kkibe@users.noreply.github.com>
2023-12-09 01:29:17 +0300

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,7 +6,31 @@ import Food from '../assets/therapy.png';
 function AboutUs() {
   return (
   <div className="container">
-    <p>Hover over the card to see the drop shadow added</p>
+    <div id="carousel-example" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li data-target="#carousel-example" data-slide-to="0" className="active"></li>
+        <li data-target="#carousel-example" data-slide-to="1"></li>
+        <li data-target="#carousel-example" data-slide-to="2"></li>
+      </ol>
+
+      <div className="carousel-inner">
+        <div className="item active">
+          <a href="#"><img src="http://placekitten.com/1600/600" /></a>
+          <div className="carousel-caption">
+            <h3>Meow</h3>
+            <p>Just Kitten Around</p>
+          </div>
+        </div>
+        <div className="item">
+          <a href="#"><img src="http://placekitten.com/1600/600" /></a>
+          <div className="carousel-caption">
+            <h3>Meow</h3>
+            <p>Just Kitten Around</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
     <div className="card-deck">
       <div className="card">
         <img className="card-img-top" src="https://dummyimage.com/600x400/000/fff" alt="Card image cap"/>
