832b70a40d85baab6080349ae974804f366253bb
Kkibe <137826199+Kkibe@users.noreply.github.com>
2023-12-08 22:21:12 +0300

    Update Cookie.jsx

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -9,7 +9,7 @@ export default function Cookie() {
   }
   return (
     <div className={`${isCookiesEnabled ? 'd-none' : 'd-flex'} align-items-center justify-content-evenly fixed-bottom w-100 p-1 bg-light`}>
-      <small>This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with our Cookie Policy.<a href="/login">Learn more about cookies</a></small>
+      <small>This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with <a href="/about">our Cookie Policy.</a></small>
       <a href='login' className="btn btn-outline-primary me-1" role='button' onClick={handleClick}>ACCEPT</a>
     </div>
   )
