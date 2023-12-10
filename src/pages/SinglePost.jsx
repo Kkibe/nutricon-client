e1b56711ed5e713de58449c21ea19aaf099e81c6
Kkibe <137826199+Kkibe@users.noreply.github.com>
2023-12-10 14:47:43 +0300

    Update SinglePost.jsx

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -13,7 +13,7 @@ function SinglePost() {
       <div>
         <h1 className='h1'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
         <div className="content d-flex align-items-center justify-center flex-column me-4 p-2">
-          <img src="" alt=""  className='m-2'/>
+          <img src="https://i.imgur.com/tPvlEdq.jpg" alt="blog-post"  className='m-2 w-100'/>
           <p className='lead'>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate eveniet accusantium culpa, animi natus id saepe error, eaque optio eos alias aut vitae placeat aliquam architecto iusto fuga modi! Saepe.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolores officia quod, incidunt accusantium obcaecati dignissimos adipisci fugiat ad porro voluptate corrupti labore illo non error? Maxime ipsum veritatis delectus?
@@ -28,6 +28,79 @@ function SinglePost() {
           </p>
         </div>
       </div>
+
+
+      <div class="container justify-content-center mt-5 border-left border-right">
+
+    <div class="d-flex justify-content-center pt-3 pb-2"> <input type="text" name="text" placeholder="+ Add a note" class="form-control addtxt"/> </div>
+
+    <div class="d-flex justify-content-center py-2">
+
+        <div class="second py-2 px-2"> <span class="text1">Type your note, and hit enter to add it</span>
+
+            <div class="d-flex justify-content-between py-1 pt-2">
+
+                <div><img src="https://i.imgur.com/AgAC1Is.jpg" width="18"/><span class="text2">Martha</span></div>
+
+                <div><span class="text3">Upvote?</span><span class="thumbup"><i class="fa fa-thumbs-o-up"></i></span><span class="text4">3</span></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="d-flex justify-content-center py-2">
+
+        <div class="second py-2 px-2"> <span class="text1">Type your note, and hit enter to add it</span>
+
+            <div class="d-flex justify-content-between py-1 pt-2">
+
+                <div><img src="https://i.imgur.com/tPvlEdq.jpg" width="18"/><span class="text2">Curtis</span></div>
+
+                <div><span class="text3">Upvote?</span><span class="thumbup"><i class="fa fa-thumbs-o-up"></i></span><span class="text4">3</span></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="d-flex justify-content-center py-2">
+
+        <div class="second py-2 px-2"> <span class="text1">Type your note, and hit enter to add it</span>
+
+            <div class="d-flex justify-content-between py-1 pt-2">
+
+                <div><img src="https://i.imgur.com/gishFbz.png" width="18" height="18"/><span class="text2">Beth</span></div>
+
+                <div><span class="text3 text3o">Upvoted</span><span class="thumbup"><i class="fa fa-thumbs-up thumbupo"></i></span><span class="text4 text4i">1</span></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="d-flex justify-content-center py-2 pb-3">
+
+        <div class="second py-2 px-2"> <span class="text1">Type your note, and hit enter to add it</span>
+
+            <div class="d-flex justify-content-between py-1 pt-2">
+
+                <div><img src="https://i.imgur.com/tPvlEdq.jpg" width="18"/><span class="text2">Curtis</span></div>
+
+                <div><span class="text3">Upvote?</span><span class="thumbup"><i class="fa fa-thumbs-o-up"></i></span><span class="text4 text4o">1</span></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+      
       <section>
         <h1 className='h1 w-100 d-flex align-center justify-center m-4'><span className="lead">Related Posts</span></h1>
         <div className="row justify-content-evenly my-3">
